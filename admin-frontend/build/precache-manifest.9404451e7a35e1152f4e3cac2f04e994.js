432c14c7ec50bca47d40e3501ca8dbcb29b91de0
Alexander Valinurov <valinurovam@gmail.com>
2019-07-03 19:35:39 +0300

    admin js fixes

---
+++ admin-frontend/build/precache-manifest.9404451e7a35e1152f4e3cac2f04e994.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4111d31fd2759226d3840e921c604792",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b5cc77837ec37604779",
+    "url": "/static/css/main.74be1e85.chunk.css"
+  },
+  {
+    "revision": "3dc5532ad67e35bcf81e",
+    "url": "/static/js/2.3d019227.chunk.js"
+  },
+  {
+    "revision": "6b5cc77837ec37604779",
+    "url": "/static/js/main.8e582e51.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
