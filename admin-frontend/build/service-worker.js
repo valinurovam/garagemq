7424a22063ca3c6662e27acae9a976268e30d7fb
Alexander Valinurov <valinurovam@gmail.com>
2021-06-07 12:13:42 +0300

    Upadte FE build after libs update

--- admin-frontend/build/service-worker.js
+++ admin-frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9404451e7a35e1152f4e3cac2f04e994.js"
+  "/precache-manifest.3afed1ea7c9ac78d7d981ba00d010ddc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
